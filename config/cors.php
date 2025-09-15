eebf23eb1aff1439dd8720a31c482d84e572a663
Papa Dieye <pvpvdieye10@gmail.com>
2025-09-15 03:30:06 +0000

    Fix CORS configuration for HTTPS frontend

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000'], // Remplacez par l'URL de votre frontend
+    'allowed_origins' => ['https://reservation-hotel-frontend-kok1.vercel.app'], // Remplacez par l'URL de votre frontend
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
