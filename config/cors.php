9a4afd41f81466556af0116bc21d66565e302605
Papa Dieye <pvpvdieye10@gmail.com>
2025-09-15 14:33:06 +0000

    Update CORS configuration for production frontend

--- config/cors.php
+++ config/cors.php
@@ -4,6 +4,7 @@ return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
     'allowed_origins' => ['https://reservation-hotel-frontend-kok1.vercel.app'], // Remplacez par l'URL de votre frontend
+    //'allowed_origins' => ['http://localhost:3000'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
